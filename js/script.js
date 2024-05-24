341451e18feced51c680755e3b63f03fb25fb078
lizijanadze <164235851+lizijanadze@users.noreply.github.com>
2024-05-24 20:55:43 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+let my_age = 13
+let my_hobby = "play_tennis"
+let my_name = "lizi"
+let year_of_birthday =2010
+
+console.log("my hobby is" +my_hobby)
+console.log("my name is" +my_name)
+console.log("my year of birthday is " +year_of_birthday)
+console.log("my age is " +my_age)
